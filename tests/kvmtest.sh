9ca724ac9b38baef250b14693e07ef31d11213cd
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2022-06-27 15:46:57 -0700

    Update kvmtest.sh

--- tests/kvmtest.sh
+++ tests/kvmtest.sh
@@ -252,7 +252,6 @@ test_multi_asic_t1_lag() {
     tgname=multi_asic_t1_lag
     tests="\
     bgp/test_bgp_fact.py \
-    snmp/test_snmp_default_route.py \
     snmp/test_snmp_loopback.py \
     snmp/test_snmp_pfc_counters.py \
     snmp/test_snmp_queue.py \
